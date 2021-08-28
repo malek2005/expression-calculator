f7236aaa0f9142483bfde2d5635edd45bebdd0f4
malek2005 <malek2005@list.ru>
2021-08-28 10:56:36 +0300

    feat:solution for task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    expr = expr.split(' ').join('');
+    
+    
+    if (expr.includes('/0')) throw Error('TypeError: Division by zero.')
+
+    let braketLeft = [];
+    let braketRight = [];
+
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] === ')') {
+            braketRight.push(expr[i])
+        } else if (expr[i] === '(') {
+            braketLeft.push(expr[i])
+        }
+    }
+
+    if (braketLeft.length != braketRight.length) {
+        throw Error('ExpressionError: Brackets must be paired');
+    }
+
+
+    return new Function(`return ${expr}`)(); 
 }
 
 module.exports = {
